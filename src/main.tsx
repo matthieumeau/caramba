6de95ba7f81eb7c1d53062bd763748450c641584
Léo <l.ploton@inlead.fr>
2024-03-26 15:00:52 +0100

    Add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import ErrorPage from './ErrorPage.tsx';
+import GameRoom from './GameRoom.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import TopBar from './TopBar.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/room/:id',
+    element: <GameRoom />,
+    errorElement: <ErrorPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TopBar />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
